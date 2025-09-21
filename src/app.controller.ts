00f2a9e2ce53bcb9bf43bb494c46bdb1b76f9431
yunusyerli1 <yunusyerli1@gmail.com>
2025-09-21 18:06:49 +0300

    Add migration config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,4 +11,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health')
+  check() {
+    return true;
+  }
 }
---
+++ typeorm.config.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+});
